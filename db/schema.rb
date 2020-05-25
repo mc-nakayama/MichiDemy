788f6838bb2c7704d488a20ff3c40074b7880d99
desktop <michitoshi.nakayama@gmail.com>
2020-05-25 14:13:59 +0000

    セミナー機能

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,15 @@ ActiveRecord::Schema.define(version: 2020_05_17_170515) do
 
   create_table "seminars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.datetime "time"
-    t.text "address"
-    t.integer "telephone"
+    t.date "date"
+    t.time "start_time"
+    t.time "end_time"
+    t.integer "subscription"
     t.integer "price"
     t.integer "cost"
     t.text "other"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
